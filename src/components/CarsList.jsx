509b1d6ff51f2df3551ce15ba90004bf8fd6e7d9
James Sherry <james.m.sherry@googlemail.com>
2023-03-01 16:11:07 +0000

    deleting functionality added

---
+++ src/components/CarsList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+// import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Typography from "@mui/material/Typography";
+
+function CarsList({
+  cars = [],
+  deleteHandler = () => console.log("No deleteHandler provided to Cars List"),
+}) {
+  return (
+    <List>
+      {cars.map(({ name, bhp, avatar_url, _id }, i) => (
+        <ListItem key={i}>
+          <ListItemAvatar>
+            <Avatar alt="" src={avatar_url} />
+          </ListItemAvatar>
+          <ListItemText>
+            {name} (BHP: {bhp})
+          </ListItemText>
+          <IconButton
+            aria-label="update"
+            to={`/update/${_id}`}
+            component={Link}
+          >
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="delete" onClick={() => deleteHandler(_id)}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+export default CarsList;
