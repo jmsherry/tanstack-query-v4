90acd437fafdd745963fca310568ce6a90280d07
James Sherry <james.m.sherry@googlemail.com>
2023-03-01 14:34:51 +0000

    Added MUI and theming

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import Button from '@mui/material/Button';
 function List() {
   return (
     <>
       <div>List</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
-    
-  )
+  );
 }
 
-export default List
+export default List;
