ed5a6bdd6c4c87785d02bf4d36714d682d4dd177
James Sherry <james.m.sherry@googlemail.com>
2023-03-08 05:59:30 +0000

    removed sleep functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,15 +10,4 @@ export const getDiffOfTwoObjects = (old, newer) => {
   }
   return diff;
 };
-
-export const sleep = (ms=2000) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export const blockingSleep = (ms=3000) => {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < ms);
-}
+ 
